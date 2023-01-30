e3252cc4bb7267e01e3b1440386e1326a69d546e
DanielMakokha <danielmakokhan@gmail.com>
2023-01-30 12:59:51 +0300

    Javascript functions

---
+++ course2/index.js
@@ -0,0 +1,14 @@
+// javascript function
+
+// function showMyname(name){
+//     console.log('My name is: ',name)
+// }
+// showMyname('Daniel');
+// showMyname('Felix');
+// showMyname('Geoffrey');
+
+function calsum(num1,num2) {
+    return num1+num2
+}
+const result=calsum(40,30);
+console.log(result)
